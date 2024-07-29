b366b4e6b7d9c6c555bbe1e82abb3657a259b008
kristrushh <kristrush@yandex.ru>
2024-07-29 13:08:43 +0300

    lesson 02/01

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -13,6 +13,13 @@ const isVerifiedUser = true
 const hasSpecialPermission = true
 const hasTemporaryPass = false
 
-let isAccess
+let isAccess 
+
 
 // your code
+
+if (isAdmin == true || isVerifiedUser == true) && (hasSpecialPermission == true || hasTemporaryPass == true) {
+    isAccess = true;
+} else {
+    isAccess = false;
+}
