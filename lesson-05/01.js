9f982d272b24bd49824ed1e88307395c2fa6f53e
kristrushh <kristrush@yandex.ru>
2024-08-24 18:55:59 +0300

    t1 w5

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,11 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
-}
+  addResource(resource, amount) {
+      if (resource in game.resources) {
+        return game.resources[resource] += amount 
+      } else {
+        console.log("Invalid resource");
+      }
+    }
+  }
