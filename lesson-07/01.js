340512241825f523809e2a4822883c3cad254cf7
kristrushh <kristrush@yandex.ru>
2024-09-02 11:57:19 +0300

    L7 - t1

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -6,4 +6,16 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(str) {
+  let arr = str.split(" ");
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    
+    newArr.push(arr[i][0].toUpperCase() + arr[i].slice(1, arr[i].length + 1));
+    
+  }
+  return newArr.join(" ")
+}
+
+
+
