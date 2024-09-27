a6f14ef467805b8f22e2bc96e6dba2645256573f
kristrushh <kristrush@yandex.ru>
2024-09-27 18:55:00 +0300

    w9-t1-t1111

--- lesson-09/01.js
+++ lesson-09/01.js
@@ -32,21 +32,39 @@ console.log(oddNumbers) // Должен вывести: [1, 3, 5]
 //   return newArr
 // }
 
-const numbers = [1, 2, 3, 4, 5]
-const filter = function (array, callback) {
-  let newArr = []
-  for (let index = 0; index < array.length; i++) {
-    if (callback(element, index)) {
-      newArr.push(element)
-  }
-}
-}
+// const numbers = [1, 2, 3, 4, 5]
+// const filter = function (array, callback) {
+//   let newArr = []
+//   for (let index = 0; index < array.length; i++) {
+//     if (element in array) {
+//       if (callback(element, index)) {
+//         newArr.push(element)
+//     }
 
-const callback = function (element, index) {
-  if (element % 2 !== 0) {
-    return index;
-  }
-}
+//     }
+
+// }
+// }
+
+// const callback = function (element, index) {
+//   if (element % 2 !== 0) {
+//     return index;
+//   }
+// }
+
+// var array = Object(this);
+//     var length = array.length >>> 0;
+//     var result = [];
+
+//     for (var i = 0; i < length; i++) {
+//       if (i in array) {
+//         if (callback.call(thisArg, array[i], i, array)) {
+//           result.push(array[i]);
+//         }
+//       }
+//     }
+
+//     return result;
 
 
 
@@ -62,6 +80,21 @@ const callback = function (element, index) {
 */
 
 
-const filter = new Function(`${this.studentCode} \n return filter;`)();
-const result = filter([1, 2, 3, 4, 5], (element, index) => element % 2 !== 0);
-(0, local_cypress_1.expect)(result).to.deep.equal([1, 3, 5], 'Должен вернуть массив с нечетными числами');
+// const filter = new Function(`${this.studentCode} \n return filter;`)();
+// const result = filter([1, 2, 3, 4, 5], (element, index) => element % 2 !== 0);
+// (0, local_cypress_1.expect)(result).to.deep.equal([1, 3, 5], 'Должен вернуть массив с нечетными числами');
+
+Array.prototype.filter = function (callback) {
+  //Store the new array
+  const result = [];
+  for (let i = 0; i < this.length; i++) {
+    //call the callback with the current element, index, and context.
+    //if it passes the text then add the element in the new array.
+    if (callback(this[i], i, this)) {
+      result.push(this[i]);
+    }
+  }
+  
+  //return the array
+  return result
+}
